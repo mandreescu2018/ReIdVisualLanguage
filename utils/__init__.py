27c20e5bc7091cd4e41b67bc2eb5c4514e7f2f49
uif86504 <mihai-traian.andreescu@continental.com>
2026-04-23 11:08:50 +0300

    init utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .set_seed import set_seeds
 from .meter import AverageMeter
 from .weight_utils import weights_init_kaiming, weights_init_classifier
 from .misc import timed
+from .device_manager import DeviceManager
